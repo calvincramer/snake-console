5ec91a7f235f31f0e4e83fa16acc18dd5f1bc972
Calvin Cramer <cjcramer@ucdavis.edu>
2019-07-13 05:55:57 +0000

    Little change

--- snake.cpp
+++ snake.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
+#include <iostream>     // C++ version io
+#include <stdio.h>      // C version io
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>     // STDOUT_FILENO definition
 
 int main()
 {
 	std::cout << "Hello snake\n";
+	struct winsize size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+	std::cout << "Console width:  " << size.ws_row << std::endl;
+	std::cout << "Console height: " << size.ws_col << std::endl;
+
 	return 0;
 }
-
